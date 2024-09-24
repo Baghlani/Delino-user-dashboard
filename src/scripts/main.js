c199145e3e40c24c04a8d05cd9ebaecca6e0ee2f
Abolfazl Baghlani <afbaghlani@yahoo.com>
2024-09-24 23:30:25 +0330

    display received data through kendo grid

--- src/scripts/main.js
+++
@@ -1,5 +0,0 @@
-import "@progress/kendo-ui/esm/kendo.grid";
-import "../styles/main.scss";
-import "./kendo-ui-license.js";
-
-$(()=>{});
---
+++ src/scripts/main.ts
@@ -0,0 +1,12 @@
+import "@progress/kendo-ui/esm/kendo.grid";
+import "../styles/main.scss";
+import { getData } from "./data";
+import "./kendo-ui-license.js";
+import initUsersGrid from "./usersGrid";
+
+const USERS_ENDPOINT = "https://jsonplaceholder.typicode.com/users";
+
+$(async () => {
+  const { data, dataSource } = await getData(USERS_ENDPOINT, 10);
+  initUsersGrid(dataSource);
+});
